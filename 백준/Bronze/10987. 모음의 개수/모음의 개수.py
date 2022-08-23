8a554b97f93e65c4f5f26b0ae99e4bf0d09fc0b0
Seung_Hun Oh (owinhun) <97443033+owinhun@users.noreply.github.com>
2022-08-23 21:01:25 +0900

    [Bronze III] Title: 모음의 개수, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.py
@@ -0,0 +1,20 @@
+word = list(input())
+cnt = 0
+
+for i in word:
+    if i == 'a':
+        cnt += 1
+    
+    if i == 'e':
+        cnt += 1
+        
+    if i == 'i':
+        cnt += 1
+        
+    if i == 'o':
+        cnt += 1
+        
+    if i == 'u':
+        cnt += 1
+        
+print(cnt)
