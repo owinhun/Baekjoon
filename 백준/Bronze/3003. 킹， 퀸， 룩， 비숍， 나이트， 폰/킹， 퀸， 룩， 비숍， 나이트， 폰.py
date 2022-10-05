eb5c615dcea1291ae46b006404849fdaac8ddbff
Seung_Hun Oh (owinhun) <97443033+owinhun@users.noreply.github.com>
2022-10-05 18:20:08 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,17 @@
+input = list(map(int, input().split()))
+
+k = 1
+q = 1
+l = 2
+b = 2
+n = 2
+p = 8
+
+k_n = k - input[0]
+q_n = q - input[1]
+l_n = l - input[2]
+b_n = b - input[3]
+n_n = n - input[4]
+p_n = p - input[5]
+
+print(k_n, q_n, l_n, b_n, n_n, p_n)
