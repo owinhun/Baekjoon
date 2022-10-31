abcbee6c7abcd7b161fa2d11ef7661092e7cbd74
Seung_Hun Oh (owinhun) <97443033+owinhun@users.noreply.github.com>
2022-10-31 16:54:09 +0900

    [Bronze III] Title: 전자레인지, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+if t % 10 != 0:
+    print(-1)
+
+else:
+    a = 0
+    b = 0
+    c = 0
+    a = t // 300
+    b = (t % 300) // 60
+    c = (t % 300) % 60 // 10
+    print(a, b, c)
