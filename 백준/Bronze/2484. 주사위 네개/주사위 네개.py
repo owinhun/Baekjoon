818ccb797b9dcd01b5aebf55647eafc7e1d53296
Seung_Hun Oh (owinhun) <97443033+owinhun@users.noreply.github.com>
2023-09-05 23:23:02 +0900

    [Bronze II] Title: 주사위 네개, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2484. 주사위 네개/주사위 네개.py
+++ 백준/Bronze/2484. 주사위 네개/주사위 네개.py
@@ -1,46 +1,41 @@
 n = int(input())
 
 max_prize = 0
-
 for i in range(n):
-    dice = list(map(int, input().split()))
-    dice_count = [0] * 7
+    n_li = [0 for j in range(7)]
+    nums = list(map(int, input().split()))
 
-    for d in dice:
-        dice_count[d] += 1
+    for d in nums:
+        n_li[d] += 1
 
-    prize = 0
+    result = 0
     for j in range(1, 7):
-        if dice_count[j] == 4:
-            prize = 50000 + j * 5000
-            break
-
-    if prize == 0:
-        for j in range(1, 7):
-            if dice_count[j] == 3:
-                prize = 10000 + j * 1000
-                break
-
-    if prize == 0:
-        for j in range(1, 7):
-            if dice_count[j] == 2:
-                for k in range(1, 7):
-                    if k != j and dice_count[k] == 2:
-                        prize = 2000 + j * 500 + k * 500
-                        break
-                if prize != 0:
-                    break
-        if prize == 0:
+        if n_li[j] == 4:
+            result = 50000 + j * 5000
+        elif n_li[j] == 3:
+            result = 10000 + j * 1000
+        elif n_li[j] == 2:
             for j in range(1, 7):
-                if dice_count[j] == 2:
-                    prize = 1000 + j * 100
-                    break
+                if n_li[j] == 2:
+                    for k in range(1, 7):
+                        if k != j and n_li[k] == 2:
+                            result = 2000 + j * 500 + k * 500
+                            break
+                    if result != 0:
+                        break
+            if result == 0:
+                for j in range(1, 7):
+                    if n_li[j] == 2:
+                        result = 1000 + j * 100
+                        break
 
-    if prize == 0:
-        max_dice = max(dice)
-        prize = max_dice * 100
+        elif n_li[j] == 2:
+            result = 1000 + j * 100
 
-    if prize > max_prize:
-        max_prize = prize
+    if result == 0:
+        max_dice = max(nums)
+        result = max_dice * 100
+
+    max_prize = max(max_prize, result)
 
 print(max_prize)
