08232ad99f9f31abcd10a2c10a97790018dfd6f5
Seung_Hun Oh (owinhun) <97443033+owinhun@users.noreply.github.com>
2023-09-11 19:37:14 +0900

    [Bronze II] Title: 주사위 네개, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2484. 주사위 네개/주사위 네개.py
+++ 백준/Bronze/2484. 주사위 네개/주사위 네개.py
@@ -12,25 +12,24 @@ for i in range(n):
     for j in range(1, 7):
         if n_li[j] == 4:
             result = 50000 + j * 5000
+
         elif n_li[j] == 3:
             result = 10000 + j * 1000
-        elif n_li[j] == 2:
-            for j in range(1, 7):
-                if n_li[j] == 2:
-                    for k in range(1, 7):
-                        if k != j and n_li[k] == 2:
-                            result = 2000 + j * 500 + k * 500
-                            break
-                    if result != 0:
-                        break
-            if result == 0:
-                for j in range(1, 7):
-                    if n_li[j] == 2:
-                        result = 1000 + j * 100
-                        break
 
         elif n_li[j] == 2:
-            result = 1000 + j * 100
+            if n_li[j] == 2:
+                for k in range(1, 7):
+                    if k != j and n_li[k] == 2:
+                        result = 2000 + j * 500 + k * 500
+                        break
+
+                    if result != 0:
+                        break
+
+            if result == 0:
+                if n_li[j] == 2:
+                    result = 1000 + j * 100
+                    break
 
     if result == 0:
         max_dice = max(nums)
